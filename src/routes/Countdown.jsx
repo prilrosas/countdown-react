c012a62512adc09b7652dbdfa927209d15e1a9fc
Priscila <prilrosas@gmail.com>
2025-04-25 15:14:23 -0400

    consumindo o contexto

--- src/routes/Countdown.jsx
+++ src/routes/Countdown.jsx
@@ -1,13 +1,25 @@
-import React from "react";
 import Title from "../components/Title";
 import Counter from "../components/Counter";
+import { Navigate } from "react-router-dom";
+
 import useCountdown from "../hooks/useCountdown";
 
+import { CountdownContext } from "../context/CountdownContext";
+
+import { useContext } from "react";
+
 const Countdown = () => {
-  const [day, hour, minute, second] = useCountdown("Jan 1, 2026 00:00:00");
+  const { event } = useContext(CountdownContext);
+
+  if (!event) return <Navigate to="/" />;
+
+  const eventTitle = event.title;
+  const eventColor = event.color;
+
+  const [day, hour, minute, second] = useCountdown(event.date);
   return (
     <>
-      <Title title="Contagem Regressiva" />
+      <Title title={eventTitle} />
       <div className="countdown-container">
         <Counter title="Dias" number={day} />
         <Counter title="Horas" number={hour} />
