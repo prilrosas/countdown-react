0ae2d4f81a0300e8b16b5de9b3297f602ca26b65
Priscila <prilrosas@gmail.com>
2025-04-25 14:49:14 -0400

    criação do contexto

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,9 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./routes/Home.jsx";
 import Countdown from "./routes/Countdown.jsx";
 
+//context
+import { CountdownProvider } from "./context/CountdownContext.jsx";
+
 //configuração de routes
 const router = createBrowserRouter([
   {
@@ -29,6 +32,8 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <CountdownProvider>
+      <RouterProvider router={router} />
+    </CountdownProvider>
   </StrictMode>
 );
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,23 +1,59 @@
-import React from "react";
+import { useContext, useState } from "react";
+
+import { CountdownContext } from "../context/CountdownContext";
+
+import { useNavigate } from "react-router-dom";
 
 import "./Home.css";
 
 const Home = () => {
+  const [title, setTitle] = useState();
+  const [date, setDate] = useState();
+  const [image, setImage] = useState();
+  const [color, setColor] = useState();
+
+  const { setEvent } = useContext(CountdownContext);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const eventObject = {
+      title,
+      date,
+      image,
+      color,
+    };
+
+    setEvent(eventObject);
+
+    // quando o evento for criado com sucesso será redirecionado parapage countdown
+    navigate("/countdown");
+  };
+
   return (
     <div className="home">
       <h2>Monte a sua contagem regressiva!</h2>
-      <form className="countdown-form">
+      <form className="countdown-form" onSubmit={handleSubmit}>
         <label>
           <span>Titulo:</span>
           <input
             type="text"
             name="title"
             placeholder="Digite o título do evento"
+            onChange={(e) => setTitle(e.target.value)}
+            required
           />
         </label>
         <label>
           <span>Data do evento:</span>
-          <input type="date" name="date" />
+          <input
+            type="date"
+            name="date"
+            onChange={(e) => setDate(e.target.value)}
+            required
+          />
         </label>
         <label>
           <span>Imagem:</span>
@@ -25,11 +61,17 @@ const Home = () => {
             type="text"
             name="image"
             placeholder="Insira o url da imagem"
+            onChange={(e) => setImage(e.target.value)}
           />
         </label>
         <label>
           <span>Cor do tema:</span>
-          <input type="color" name="Color" />
+          <input
+            type="color"
+            name="Color"
+            onChange={(e) => setColor(e.target.value)}
+            required
+          />
         </label>
         <input type="submit" value="Enviar" />
       </form>
